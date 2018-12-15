d0930a9d32c045e7f44bae60c3023134fd8edb84
Em <emiton_alves@my.uri.edu>
2018-12-15 13:26:39 -0500

    Redid execute using bitpack

--- execute.c
+++ execute.c
@@ -15,9 +15,9 @@
 //                           int registerPositions[],
 //                           uint32_t *registerValues);
 
-void getRegisterValues(uint32_t word,
-                           int registerPositions[],
-                           uint32_t **registerValues);
+//void getRegisterValues(uint32_t word,
+//                           int registerPositions[],
+//                           uint32_t **registerValues);
 
 int instructionPointer = 0;
 uint32_t registersGP[8] = {0};
@@ -41,72 +41,89 @@ void runProgram(struct segment *instructionSet)
         uint32_t currentWord = currentSegment->segmentWords[instructionPointer];
         uint32_t operator = (uint32_t)Bitpack_getu(currentWord, 4, 28); // look at casting and other methods. Is this safe?
         //uint32_t registerValues[3] = {0}; // Registers that will be operated on 
-        uint32_t *registerValues;
-        int abcPositions[3] = {6,3,0};
+        //uint32_t *registerValues;
+        uint32_t A, B, C;
+        /*int abcPositions[3] = {6,3,0};
         int bcPositions[3] = {-1,3,0};
         int cPosition[3] = {-1,-1,0};
-        int aPosition[3] = {25,-1,-1};
+        int aPosition[3] = {25,-1,-1};*/
         switch(operator)
         {
-            case 0: 
-                getRegisterValues(currentWord, abcPositions, &registerValues);
-                conditionalMove(registerValues, registersGP);
+            case 0:
+                A = Bitpack_getu(currentWord, 3, 6);
+                B = Bitpack_getu(currentWord, 3, 3);
+                C = Bitpack_getu(currentWord, 3, 0); 
+                conditionalMove(A, B, C, registersGP);
                 break;
             case 1:
-                getRegisterValues(currentWord, abcPositions, &registerValues);
-                segmentedLoad(registerValues, registersGP, memory);
+                A = Bitpack_getu(currentWord, 3, 6);
+                B = Bitpack_getu(currentWord, 3, 3);
+                C = Bitpack_getu(currentWord, 3, 0);
+                segmentedLoad(A, B, C, registersGP, memory);
                 break;
             case 2:
-                getRegisterValues(currentWord, abcPositions, &registerValues);
-                segmentedStore(registerValues, registersGP, memory);
+                A = Bitpack_getu(currentWord, 3, 6);
+                B = Bitpack_getu(currentWord, 3, 3);
+                C = Bitpack_getu(currentWord, 3, 0);
+                segmentedStore(A, B, C, registersGP, memory);
                 break;
             case 3:
-                getRegisterValues(currentWord, abcPositions, &registerValues); 
-                addition(registerValues, registersGP);
+                A = Bitpack_getu(currentWord, 3, 6);
+                B = Bitpack_getu(currentWord, 3, 3);
+                C = Bitpack_getu(currentWord, 3, 0);
+                addition(A, B, C, registersGP);
                 break;
             case 4:
-                getRegisterValues(currentWord, abcPositions, &registerValues);
-                multiplication(registerValues, registersGP);
+                A = Bitpack_getu(currentWord, 3, 6);
+                B = Bitpack_getu(currentWord, 3, 3);
+                C = Bitpack_getu(currentWord, 3, 0);
+                multiplication(A, B, C, registersGP);
                 break;
             case 5:
-                getRegisterValues(currentWord, abcPositions, &registerValues);
-                division(registerValues, registersGP);
+                A = Bitpack_getu(currentWord, 3, 6);
+                B = Bitpack_getu(currentWord, 3, 3);
+                C = Bitpack_getu(currentWord, 3, 0);
+                division(A, B, C, registersGP);
                 break;
             case 6:
-                getRegisterValues(currentWord, abcPositions, &registerValues);
-                bitwiseNAND(registerValues, registersGP);
+                A = Bitpack_getu(currentWord, 3, 6);
+                B = Bitpack_getu(currentWord, 3, 3);
+                C = Bitpack_getu(currentWord, 3, 0);
+                bitwiseNAND(A, B, C, registersGP);
                 break;
             case 7:
                 halt(unmappedSegs, memory);
                 break;
             case 8:
-                getRegisterValues(currentWord, bcPositions, &registerValues);
-                mapSegment(registerValues, registersGP, unmappedSegs, memory);
+                B = Bitpack_getu(currentWord, 3, 3);
+                C = Bitpack_getu(currentWord, 3, 0);
+                mapSegment(B, C, registersGP, unmappedSegs, memory);
                 break;
             case 9:
-                getRegisterValues(currentWord, cPosition, &registerValues);
-                unmapSegment(registerValues, registersGP, unmappedSegs, memory);
+                C = Bitpack_getu(currentWord, 3, 0);
+                unmapSegment(C, registersGP, unmappedSegs, memory);
                 break;
             case 10:
-                getRegisterValues(currentWord, cPosition, &registerValues);
-                output(registerValues, registersGP);
+                C = Bitpack_getu(currentWord, 3, 0);
+                output(C, registersGP);
                 break;
             case 11:
-                getRegisterValues(currentWord, cPosition, &registerValues);
-                input(registerValues, registersGP);
+                C = Bitpack_getu(currentWord, 3, 0);
+                input(C, registersGP);
                 break;
             case 12:
-                getRegisterValues(currentWord, bcPositions, &registerValues);
-                loadProgram(registerValues, 
+                B = Bitpack_getu(currentWord, 3, 3);
+                C = Bitpack_getu(currentWord, 3, 0);
+                loadProgram(B,
+                            C, 
                             registersGP, 
                             memory, 
                             &instructionPointer, 
                             &instructionCount);
                 break;
             case 13:
-                getRegisterValues(currentWord, aPosition, &registerValues);
-                uint32_t value = Bitpack_getu(currentWord, 0, 25);
-                loadValue(registerValues, registersGP, value);
+                C = Bitpack_getu(currentWord, 3, 25);
+                loadValue(C, registersGP, value);
                 break;
         } 
     }
@@ -129,6 +146,7 @@ uint32_t* getRegisterValues(uint32_t word,
     return *regVal;
 }
 */
+/*
 void getRegisterValues(uint32_t word,
                            int registerPositions[],
                            uint32_t **registerValues) 
@@ -144,3 +162,4 @@ void getRegisterValues(uint32_t word,
     }
     *registerValues = regVal;
 }
+*/
